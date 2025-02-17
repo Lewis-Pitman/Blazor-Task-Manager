f4dd1bafb2a8dbfab5bf2b9ef29f52eac3e05365
Lewis-Pitman <lpitman08@outlook.com>
2025-02-17 22:02:40 +0000

    String shortening
    If the title or description of a task is too long, it will become shortened on both the dashboard under "todays tasks" and on the datagrid. Does not apply to kanban

---
+++ Helpers/StringHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorTaskManager.Helpers
+{
+    public class StringHelper
+    {
+        public static string ShortenString(string targetString, string typeOfString, int cutOffAmount)
+        {
+            if (targetString != null)
+            {
+                if (targetString.Length > cutOffAmount)
+                {
+                    targetString = targetString.Substring(0, cutOffAmount - 3) + "...";
+                }
+
+                return targetString;
+            }
+            else
+            {
+                return string.Concat("No ", typeOfString);
+            }
+        }
+    }
+}
